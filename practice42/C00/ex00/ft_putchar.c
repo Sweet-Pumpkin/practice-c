eff62725b540b18c49d11ba8d863aa40b64db42f
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-08-09 18:45:41 +0900

    Pratice 42 C00

--- practice42/C00/ex00/ft_putchar.c
+++ practice42/C00/ex00/ft_putchar.c
@@ -1,9 +1,31 @@
+/* 
+
+****** Exercise 00 ******
+ft_putchar
+Turn-in directory : ex00/
+Files to turn in : ft_putchar.c
+Allowed functions: write
+
+- 매개변수로 전달되는 문자를 표시하는 문자를 작성하세요.
+- 프로토타입은 다음과 같이 선언합니다. : 
+
+  void ft_putchar(char c);
+
+  문자를 표시하려면 다음과 같이 wirte 함수를 사용해야 합니다.
+
+  wirte(1, &c, 1);
+
+*/
+
+/* 제출답안 */
 #include <unistd.h>
 
 void ft_putchar(char c) {
   write(1, &c, 1);
 }
 
+/* 답안확인 */
 int main(void) {
   ft_putchar('a');
+  return 0;
 }
--- practice42/C00/ex01/ft_print_alphabet.c
+++ practice42/C00/ex01/ft_print_alphabet.c
@@ -1,3 +1,19 @@
+/* 
+
+****** Exercise 01 ******
+ft_print_alphabet
+Turn-in directory : ex01/
+Files to turn in : ft_print_alphabet.c
+Allowed functions: write
+
+- 알파벳을 소문자로 'a'부터 순서대로 한 줄로 표시하는 함수를 작성하세요.
+- 프로토타입은 다음과 같이 선언합니다. : 
+
+  void ft_print_alphabet(void);
+
+*/
+
+/* 제출답안 */
 #include <unistd.h>
 
 void ft_print_alphabet(void) {
@@ -9,6 +25,8 @@ void ft_print_alphabet(void) {
 	}
 }
 
+/* 답안확인 */
 int main(void) {
 	ft_print_alphabet();
+	return 0;
 }
