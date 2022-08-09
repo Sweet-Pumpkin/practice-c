f2dcb55db90c3d237575cf19470c8b0d10901655
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-08-09 22:40:12 +0900

    Practice 42 C00

--- practice42/C00/ex06/ft_print_comb2.c
+++ practice42/C00/ex06/ft_print_comb2.c
@@ -21,32 +21,28 @@ Allowed functions: write
 /* 제출답안 */
 #include <unistd.h>
 
-void ft_write(char a) {
-  write(1, &a, 1);
-}
-
-void getNumbers(int a, int b) {
-  ft_write(48 + a / 10);
-  ft_write(48 + a % 10);
-  ft_write(' ');
-  ft_write(48 + b / 10);
-  ft_write(48 + b % 10);
-  if (a + b != 98 + 99) {
-    ft_write(',');
-    ft_write(' ');
-  }
+void ft_putchar(char c) {
+  write(1, &c, 1);
 }
 
 void ft_print_comb2(void) {
   int a, b;
-  a = 0;
-  while (a <= 99) {
-    b = a + 1;
-    while (b <= 99) {
-      getNumbers(a, b);
-      b++;
+
+  a = -1;
+
+  while(a++ < 98) {
+    b = a;
+    while (b++ < 99) {
+      ft_putchar((a / 10) + '0');
+      ft_putchar((a % 10) + '0');
+      ft_putchar(' ');
+      ft_putchar((b / 10) + '0');
+      ft_putchar((b % 10) + '0');
+      if (a != 99) {
+        ft_putchar(',');
+        ft_putchar(' ');
+      }
     }
-    a++;
   }
 } 
 
