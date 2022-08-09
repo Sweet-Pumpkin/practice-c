bd549e369d60646fb7fdf0d83e7873199d55ab8a
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-08-09 22:16:14 +0900

    Practice 42 C00

--- practice42/C00/ex05/ft_print_comb.c
+++ practice42/C00/ex05/ft_print_comb.c
@@ -22,27 +22,33 @@ Allowed functions: write
 
 /* 제출답안 */
 #include <unistd.h>
+void ft_putchar(char c) {
+  write(1, &c, 1);
+}
 
 void ft_print_comb(void) {
-  char num[5];
-  num[0] = '0';
-  num[3] = ',';
-  num[4] = ' ';
+  char num1, num2, num3;
 
-  while (num[0] <= '6') {
-    num[1] = num[0] + 1;
-    while (num[1] <= '8') {
-      num[2] = num[1] + 1;
-      while (num[2] <= '9') {
-        write(1, num, 5);
-        num[2]++;
+  num1 = '0';
+  while (num1 <= '7') {
+    num2 = num1 + 1;
+    while (num2 <= '8') {
+      num3 = num2 + 1;
+      while (num3 <= '9') {
+        ft_putchar(num1);
+        ft_putchar(num2);
+        ft_putchar(num3);
+
+        if (num1 != '7') {
+          ft_putchar(',');
+          ft_putchar(' ');
+        }
+        num3++;
       }
-      num[1]++;
+      num2++;
     }
-    num[0]++;
+    num1++;
   }
-  
-  write(1, "789", 3);
 } 
 
 /* 답안확인 */
